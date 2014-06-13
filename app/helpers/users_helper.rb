7b5aa3bfc085fb328df8b21be6dac9d55a85043c
ehaque <ehaque@gmail.com>
2014-06-13 16:43:47 -0700

    finished ch 7

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+	# Returns the Gravatar (http://gravatar.com/) for the given user.
+	def gravatar_for(user)
+		gHash = Digest::MD5::hexdigest(user.email.downcase)
+		gUrl = "https://secure.gravatar.com/avatar/#{gHash}?d=wavatar"
+		image_tag(gUrl, alt: user.name, class: "gravatar")
+
+	end
+
 end
