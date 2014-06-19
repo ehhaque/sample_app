f78e0053d53aec262949f9ecf6ebee4e293eac49
ehhaque <ehaque@gmail.com>
2014-06-18 18:01:11 -0700

    xx

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User < ActiveRecord::Base
 	before_create :create_remember_token
 
 	validates :name,  presence: true, length: { maximum: 50 }
+	validates :password,  presence: true, length: { minimum: 6 }
+	validates :password_confirmation,  presence: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+    
+
+
+
 
     has_secure_password
 
