7b4a8624bec6d8e071243d79f1d8bdd420d576bc
ehhaque <ehaque@gmail.com>
2014-06-19 17:32:45 -0700

    finsish chap 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,   only: [:destroy]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "User Deleted"
+    redirect_to users_url
+  end
+
   def new
   	@user = User.new
   end
@@ -13,8 +29,6 @@ class UsersController < ApplicationController
   	else
   		render 'new'
   	end
-
-
   end
 
   def show
@@ -37,10 +51,25 @@ class UsersController < ApplicationController
 
 
   private 
-  def user_params
-  	params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-  end
+    def user_params
+    	params.require(:user).permit(:name, :email, :password,
+                                     :password_confirmation)
+    end
 	
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,13 @@
 module UsersHelper
 
 	# Returns the Gravatar (http://gravatar.com/) for the given user.
-	def gravatar_for(user)
-		gHash = Digest::MD5::hexdigest(user.email.downcase)
-		gUrl = "https://secure.gravatar.com/avatar/#{gHash}?d=wavatar"
-		image_tag(gUrl, alt: user.name, class: "gravatar")
-
-	end
+	def gravatar_for(user, options = { size: 50 })
+	    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+	    size = options[:size]
+	    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=wavatar"
+	    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  	end
 
 end
+
+
