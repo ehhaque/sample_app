c9872c880b36f22b0d7ccb809a4cabef85092ca9
ehhaque <ehaque@gmail.com>
2014-06-20 17:37:45 -0700

    finished chap 10

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
   
+  get "microposts/create"
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   #get "users/new"
 
   root  'static_pages#home'
